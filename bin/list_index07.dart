95e9987758ffeca9e6372c44701fa3c95b206808
Rustambek <rustambeksafarovv@gmail.com>
2022-07-12 17:31:36 +0500

    Redone

--- bin/list_index07.dart
+++ bin/list_index07.dart
@@ -6,4 +6,22 @@
     Returns:
         list: return answer
 */
+List func(List list1){
+    List func( List list1){
+    int x = -1;
+    int y = list1.length - 1;
+    bool z = false;
+    while(x < y){
+        x++;
+        if(list1[x]==0){
+            list1[x]= z;
+
+
+        }
+    }
+    
+    return list1;
+    
+}
+}
 void main() {}
